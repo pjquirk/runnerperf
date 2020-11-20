9c4ccfb029ad577b6963a0c11ba7dbc688aa0460
PJ Quirk <paquirk@microsoft.com>
2020-11-20 18:44:19 -0500

    Added disk data

--- script/aggregate.sh
+++ script/aggregate.sh
@@ -13,8 +13,7 @@ csvFile="$destDir/summary.csv"
 if [[ -f "$csvFile" ]]; then
     rm "$csvFile"
 fi
-echo "Repository,Attempt,Runner,Build Time,CPU,CPU 95%,Disk IOPS,Disk IOPS 95%,Disk MB/s,Disk MB/s 95%,Available Memory,Available Memory 95%" > "$csvFile"
-
+echo "Repository,Attempt,Runner,Build Time,CPU,CPU 95%,Disk Read IOPS,Disk Read IOPS 95%,Disk Write IOPS,Disk Write IOPS 95%,Disk Read MB/s,Disk Read MB/s 95%,Disk Write MB/s,Disk Write MB/s 95%,Available Memory,Available Memory 95%" > "$csvFile"
 
 shopt -s nullglob
 for cpu in $sourceDir/*-iostat_cpu.csv
@@ -27,14 +26,33 @@ do
     # extract into CSV:
     # Repository,Attempt,Build Time,CPU,CPU 95%,Disk IOPS,Disk IOPS 95%,Disk MB/s,Disk MB/s 95%,Available Memory,Available Memory 95%
 
-    vals=$(cat $cpu | datamash --header-in --sort --field-separator=, mean 2 perc:90 2)
+    vals=$(cat $cpu | datamash --header-in --sort --field-separator=, mean 2 perc:95 2)
     cpu_mean=${vals%,*}
     cpu_P95=${vals#*,}
 
     buildTime=$(cat $timing)
 
-    # find the most active device
     # headers:
+    #   1 datetime
+    #   2 device
+    #   3 Device
+    #   4 r/s
+    #   5 w/s
+    #   6 rMB/s
+    #   7 wMB/s
+    #   8 rrqm/s
+    #   9 wrqm/s
+    #   10 %rrqm
+    #   11 %wrqm
+    #   12 r_await
+    #   13 w_await
+    #   14 aqu-sz
+    #   15 rareq-sz
+    #   16 wareq-sz
+    #   17 svctm
+    #   18 %util
+
+    # headers (alt):
     #   1 datetime,
     #   2 device,
     #   3 Device,
@@ -59,19 +77,34 @@ do
     #   22 aqu-sz,
     #   23 %util
 
-    #datamash --header-in --sort --field-separator=, sum 4 sum 10
+    # Check for the alternate headers
+    columns="4,6,5,7"
+    if grep -q "dareq" $devices; then
+        columns="4,5,10,11"
+    fi
 
-    #vals=$(cat $devices | datamash --header-in --sort --field-separator=, mean 2 perc:90 2)
-    diskIO_mean=""
-    diskIO_P95=""
+    # Find the most active device
+    # Linux
+    deviceData=$(cat $devices | datamash --header-in --sort --field-separator=, mean $columns perc:95 $columns --group=2 | sort -nr -k2 -t, | head --lines=1)
+    # Mac
+    # deviceData=$(cat $devices | datamash --header-in --sort --field-separator=, mean $columns perc:95 $columns --group=2 | sort -nr -k2 -t, | head -n 1)
 
-    #vals=$(cat $devices | datamash --header-in --sort --field-separator=, mean 2 perc:90 2)
-    diskMBs_mean=""
-    diskMBs_P95=""
+    # Values is: device, rmean,rmbmean,wmean,wmbmean, rp95,rmbp95,wp95,wmbp95
+    #                        2       3     4       5     6      7    8      9
+    diskIO_r_mean=$(echo $deviceData | cut -d, -f 2)
+    diskIO_r_P95=$(echo $deviceData | cut -d, -f 6)
+    diskIO_w_mean=$(echo $deviceData | cut -d, -f 4)
+    diskIO_w_P95=$(echo $deviceData | cut -d, -f 8)
+
+    #vals=$(cat $devices | datamash --header-in --sort --field-separator=, mean 2 perc:95 2)
+    diskMBs_r_mean=$(echo $deviceData | cut -d, -f 3)
+    diskMBs_r_P95=$(echo $deviceData | cut -d, -f 7)
+    diskMBs_w_mean=$(echo $deviceData | cut -d, -f 5)
+    diskMBs_w_P95=$(echo $deviceData | cut -d, -f 9)
 
     availMem_mean=""
     availMem_P95=""
 
-    line="$repo,$attempt,$runner,$buildTime,$cpu_mean,$cpu_P95,$diskIO_mean,$diskIO_P95,$diskMBs_mean,$diskMBs_P95,$availMem_mean,$availMem_P95"
+    line="$repo,$attempt,$runner,$buildTime,$cpu_mean,$cpu_P95,$diskIO_r_mean,$diskIO_r_P95,$diskIO_w_mean,$diskIO_w_P95,$diskMBs_r_mean,$diskMBs_r_P95,$diskMBs_w_mean,$diskMBs_w_P95,$availMem_mean,$availMem_P95"
     echo "$line" >> "$csvFile"
 done
